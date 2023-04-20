59baf95f4a86df845e3d29bcd5153ee7ce0eb772
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-04-20 21:06:24 +0100

    extra code deleted

--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,8 @@
 const User = require('./User');
-<<<<<<< HEAD
+
 const Recipe = require('./Recipe');
 const Category = require('./Category');
 const Order = require('./Order');
 
 module.exports = { User, Recipe, Category, Order };
-=======
-const Product = require('./Product');
-const Category = require('./Category');
-const Order = require('./Order');
 
-module.exports = { User, Product, Category, Order };
->>>>>>> master
