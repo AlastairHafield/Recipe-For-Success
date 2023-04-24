b295cd843f1025a4a91d42b94c6ac64c738fa6e7
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-24 17:18:39 +0100

    fixing bugs

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,13 +5,11 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Spices' },
-    { name: 'Vegetables' },
-    { name: 'Fruit' },
-    { name: 'Meat' },
-    { name: 'Seafood' },
-    { name: 'Dairy' },
-    { name: 'Dry goods' },
+    { name: 'Food' },
+    { name: 'Household Supplies' },
+    { name: 'Electronics' },
+    { name: 'Books' },
+    { name: 'Toys' }
   ]);
 
   console.log('categories seeded');
@@ -20,111 +18,104 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
-      description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
-      category: categories[0]._id,
-      price: 4.99,
-      quantity: 123
-    },
-    {
-      name: 'Jar of Coffee',
+      name: 'Canned Coffee',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'coffeejar.jpg',
+      image: 'canned-coffee.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 646
+      price: 1.99,
+      quantity: 500
     },
     {
-      name: 'pork sausages 500g',
+      name: 'Toilet Paper',
       category: categories[1]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'Sausages.jpg',
-      price: 3.80,
-      quantity: 47
+      image: 'toilet-paper.jpg',
+      price: 7.99,
+      quantity: 20
     },
     {
-      name: 'Rump steack 200g',
+      name: 'Handmade Soap',
       category: categories[1]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'steak.jpg',
-      price: 5.99,
-      quantity: 25
+      image: 'soap.jpg',
+      price: 3.99,
+      quantity: 50
     },
     {
-      name: 'Carrots 500g',
-      category: categories[2]._id,
+      name: 'Set of Wooden Spoons',
+      category: categories[1]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'carrots.jpg',
-      price: 2.00,
-      quantity: 106
+      image: 'wooden-spoons.jpg',
+      price: 14.99,
+      quantity: 100
     },
     {
-      name: 'Cayenne Pepper 50g',
-      category: categories[3]._id,
+      name: 'Camera',
+      category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'spicejar.jpg',
-      price: 1.99,
-      quantity: 26
+      image: 'camera.jpg',
+      price: 399.99,
+      quantity: 30
     },
     {
-      name: 'Salmon fillet 150g',
-      category: categories[4]._id,
+      name: 'Tablet',
+      category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tSalmon.jpg',
-      price: 3.50,
-      quantity: 12
+      image: 'tablet.jpg',
+      price: 199.99,
+      quantity: 30
     },
     {
-      name: 'Prawns 200g',
-      category: categories[4]._id,
+      name: 'Tales at Bedtime',
+      category: categories[3]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'Prawns.jpg',
-      price: 4.99,
-      quantity: 4
+      image: 'bedtime-book.jpg',
+      price: 9.99,
+      quantity: 100
     },
     {
-      name: 'Banana 400g',
-      category: categories[5]._id,
+      name: 'Spinning Top',
+      category: categories[4]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'Banana.jpg',
+      image: 'spinning-top.jpg',
       price: 1.99,
-      quantity: 49
+      quantity: 1000
     },
     {
-      name: 'Butter 250g',
-      category: categories[6]._id,
+      name: 'Set of Plastic Horses',
+      category: categories[4]._id,
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'Butter.jpg',
-      price: 2.35,
-      quantity: 41
+      image: 'plastic-horses.jpg',
+      price: 2.99,
+      quantity: 1000
     },
     {
-      name: 'Roquefort 110g',
-      category: categories[6]._id,
+      name: 'Teddy Bear',
+      category: categories[4]._id,
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'Cheese',
-      quantity: 5
+      image: 'teddy-bear.jpg',
+      price: 7.99,
+      quantity: 100
     },
     {
-      name: 'Greek Yogurt 300ml',
+      name: 'Alphabet Blocks',
       category: categories[4]._id,
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'Yogurt.jpg',
-      price: 1.80,
-      quantity: 86
-    }
+      image: 'alphabet-blocks.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    
   ]);
 
   console.log('products seeded');
@@ -132,10 +123,10 @@ db.once('open', async () => {
   await User.deleteMany();
 
   await User.create({
-    firstName: 'Captain Jean-Luc',
-    lastName: 'Picard',
-    email: 'picard@testmail.com',
-    password: 'password123456',
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
     orders: [
       {
         products: [products[0]._id, products[0]._id, products[1]._id]
@@ -144,10 +135,10 @@ db.once('open', async () => {
   });
 
   await User.create({
-    firstName: 'Elvis',
-    lastName: 'Presley',
-    email: 'elvist@testmail.com',
-    password: 'password123456'
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345'
   });
 
   console.log('users seeded');
