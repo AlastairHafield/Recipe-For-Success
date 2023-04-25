b318fe4aaa5a8531292ea553982906d3d95e9758
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-25 20:07:02 +0100

    adding code for reviews

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,17 +1,26 @@
-import React, { useState} from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Reviews = () => {
-    const [name, setName] = usseState('');
-    const [email, setEmail] = useState('');
-    const [rating, setRating]= useState(0);
-    const [comment, setComment]= useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        axios.post('/api/reviews', {name, email, rating,
-        .then(res => console.log(res.data))
-    .catch(err => console.log(err));
-})
-    }
-}
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post('/api/reviews', { name, email, rating, comment })
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+        <h1>Leave a Review</h1>
+        <form onSubmit={handleSubmit}></form>
+        <div>
+            <label>Name</label>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}
+        </div>
+    </div>
+  )
