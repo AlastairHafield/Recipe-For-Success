be3dfbb3e256d1395988a637839ee74b64e49a74
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-25 19:58:16 +0100

    creating reviews.js

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,3 +1,17 @@
 import React, { useState} from "react";
 import axios from 'axios';
 
+const Reviews = () => {
+    const [name, setName] = usseState('');
+    const [email, setEmail] = useState('');
+    const [rating, setRating]= useState(0);
+    const [comment, setComment]= useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post('/api/reviews', {name, email, rating,
+        .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+})
+    }
+}
