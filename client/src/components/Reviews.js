672c984e377277625c4724cb8b271ee86bf65c0a
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-25 20:39:16 +0100

    fixing more bugs

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -16,36 +16,36 @@ const Reviews = () => {
 
   return (
     <div>
-        <h1>Leave a Review</h1>
-        <form onSubmit={handleSubmit}>
+      <h1>Leave a Review</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-            <label>Name</label>
-            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+          <label>Name:</label>
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
         </div>
-    <div>
-        <label> Email</label>
-        <label>Email:</label>
+        <div>
+          <label>Email:</label>
           <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
         </div>
         <div>
-            <label> Rating</label>
-            <select value={rating} onChange={e => setEmail(e.target.value)}/>
-            <option value ="0">Select a rating</option>
-            <option value ="1">1 </option>
+          <label>Rating:</label>
+          <select value={rating} onChange={e => setRating(e.target.value)}>
+            <option value="0">Select a rating</option>
+            <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
-            </select>
-            </div>
-            <div>
-                <label>Comment</label>
-                <textarea value={comment} onChange=(e.=> setComment(e.target.value))></textarea>               
+          </select>
+        </div>
+        <div>
+          <label>Comment:</label>
+          <textarea value={comment} onChange={e => setComment(e.target.value)}></textarea>
         </div>
         <button type="submit">Submit Review</button>
-        </form>
-        </div>
+      </form>
+    </div>
   );
 };
 
-export default Reviews
+export default Reviews;
+
