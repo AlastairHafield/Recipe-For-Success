48a927c118361570e8adfb0921c5c3471ea5a1aa
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-27 19:26:32 +0100

    additional code

--- server/models/Reviews.js
+++ server/models/Reviews.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 const reviewSchema = new Schema({
-  name; { typeKey:String, required: true},
-    
-})
+
+  name: { type: String, required: true},
+  email: {type: String, required: true},
+  rating: {type: Number, required: true},
+  comment: {type: String, required: true},
+
+});
+
+const Review 
