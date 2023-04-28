fc779347090334c5388a04d54f63d6c703e59ed1
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-29 00:45:57 +0100

    updating models index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,7 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
+const Reviews = require('./Reviews');
+const Rating = require('./Rating');
 
-module.exports = { User, Product, Category, Order,Reviews};
+module.exports = { User, Product, Category, Order, Reviews, Rating};
