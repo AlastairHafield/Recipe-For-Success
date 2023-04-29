aaf9e7ed17834ef8ba7972e5d08d269a4701bf88
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-29 11:10:23 +0100

    changes

--- server/server.js
+++ server/server.js
@@ -78,5 +78,6 @@ db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
   
   });
