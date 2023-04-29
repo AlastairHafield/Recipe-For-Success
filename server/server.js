c17d1b0e51448668ae113973d05c9dd0c2e2bda6
SHAJLP <sophiaaltaf73@gmail.com>
2023-04-29 11:04:27 +0100

    adding additional code to server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
-<<<<<<< HEAD
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+=======
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -9,15 +16,7 @@ const bodyParser = require ('body-parser');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-=======
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
-const { authMiddleware } = require("./utils/auth");
-
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
->>>>>>> 6c939bf (apollo issues solved, jwt working, stripe working)
+>>>>>>> ace7302 (adding additional code to server.js)
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -84,15 +83,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-<<<<<<< HEAD
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-  
-  });
-=======
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -115,4 +105,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
->>>>>>> 6c939bf (apollo issues solved, jwt working, stripe working)
